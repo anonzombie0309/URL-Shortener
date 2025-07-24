03a48f3a949bc1055b83cc94e8c4b42609987971
anonzombie0309 <aviralbandooni@gmail.com>
2025-07-24 21:53:00 +0530

    Describe what you changed or fixed

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,10 @@
 import pytest
 from app.main import app
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 @pytest.fixture
 def client():
