e48c9244f71e89cd8ee7748debf365871c88f7c6
anonzombie0309 <aviralbandooni@gmail.com>
2025-07-24 21:57:45 +0530

    tested

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,16 +1,12 @@
 import pytest
 from app.main import app
-import sys
-import os
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
-        yield client
+          yield client
 
 def test_health_check(client):
     response = client.get('/')
